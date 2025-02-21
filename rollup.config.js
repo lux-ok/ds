1a17eed23128a3f468f17d3889ac10fda02ee7c0
Thomas Choi <104286283+thomaschoi721@users.noreply.github.com>
2025-02-21 15:55:09 +0800

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -4,32 +4,29 @@ import typescript from "@rollup/plugin-typescript";
 
 export default {
   input: {
-    index: "src/index.ts", // 主入口文件
-    "func/index": "src/func/index.ts", // func 目录单独作为一个入口
+    index: "src/index.ts",
+    "func/index": "src/func/index.ts",
   },
   output: [
     {
-      // 对应的 ESM 输出
       dir: "dist",
       format: "esm",
       sourcemap: false,
-      entryFileNames: "[name].js", // 生成 func 和 main 对应的 JS 文件
-      // chunkFileNames: "[name]-[hash].js",
+      entryFileNames: "[name].js",
     },
     {
-      // 对应的 CJS 输出
       dir: "dist",
       format: "cjs",
       sourcemap: false,
       entryFileNames: "[name].cjs",
       exports: "named",
-      // chunkFileNames: "[name]-[hash].cjs",
     },
   ],
   plugins: [
     del({ targets: "dist/**/*", hook: "buildStart" }),
     typescript({
       tsconfig: "./tsconfig.json",
+      isolatedModules: true,
     }),
   ],
   external: [],
